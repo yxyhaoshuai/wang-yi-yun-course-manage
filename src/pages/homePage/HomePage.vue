f521a5d9c2dd94c065d8af53d8dd650b4598ce83
yanxiuyu <173198977@qq.com>
2024-02-14 04:18:51 +0800

    安装样式预处理器

--- src/pages/homePage/HomePage.vue
+++ src/pages/homePage/HomePage.vue
@@ -1,53 +1,14 @@
 <template>
   <div class="home-layout">
     <div class="left">
-      <el-menu
-          active-text-color="#ffd04b"
-          background-color="#545c64"
-          class="el-menu-vertical-demo"
-          default-active="2"
-          text-color="#fff"
-          @open="handleOpen"
-          @close="handleClose"
-          :collapse="count"
-      >
-        <el-sub-menu index="1">
-          <template #title>
-            <el-icon><Location /></el-icon>
-            <span>Navigator One</span>
-          </template>
-          <el-menu-item-group title="Group One">
-            <el-menu-item index="1-1">item one</el-menu-item>
-            <el-menu-item index="1-2">item two</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-sub-menu index="1-4">
-            <template #title>item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-sub-menu>
-        </el-sub-menu>
-        <el-menu-item index="2">
-          <el-icon><Menu /></el-icon>
-          <span>Navigator Two</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <el-icon><document /></el-icon>
-          <span>Navigator Three</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <el-icon><setting /></el-icon>
-          <span>Navigator Four</span>
-        </el-menu-item>
-      </el-menu>
+
     </div>
     <div class="right">
       <div class="header">
 
       </div>
       <div class="manage-body">
-        内容
+        内容m
       </div>
     </div>
   </div>
@@ -56,21 +17,14 @@
 
 <script>
 
-import {Menu} from "@element-plus/icons-vue";
 import {ref} from "vue";
 export default {
   name: 'MyComponents',
-  components: {Menu},
   props: {
     msg: String
   },
   methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
-    }
+
   },
   setup() {
     const count = ref(false)
